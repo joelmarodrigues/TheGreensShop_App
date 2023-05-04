d12b1daa2a87694d6a42534c87151df625e719cc
Joelma Rodrigues <113507468+joelmarodrigues@users.noreply.github.com>
2023-05-04 20:17:28 +0100

    firebase and firestore successfully integrated :D

--- app/src/main/java/com/example/thegreensshop_app/LoginActivity.kt
+++ app/src/main/java/com/example/thegreensshop_app/LoginActivity.kt
@@ -1,19 +1,25 @@
 package com.example.thegreensshop_app
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var emailEditText: EditText
     private lateinit var passwordEditText: EditText
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        mAuth = FirebaseAuth.getInstance()
 
         emailEditText = findViewById(R.id.email_edittext)
         passwordEditText = findViewById(R.id.password_edittext)
@@ -24,16 +30,39 @@ class LoginActivity : AppCompatActivity() {
             val email = emailEditText.text.toString().trim()
             val password = passwordEditText.text.toString().trim()
 
-            // TODO: Implement login logic
+            if (email.isEmpty() || password.isEmpty()) {
+                emailEditText.error = "Email or Password is empty"
+                emailEditText.requestFocus()
+                return@setOnClickListener
+            }
 
+            mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success")
+
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
         }
 
         signupButton.setOnClickListener {
-            loginButton.setOnClickListener {
-                val intent = Intent(this, LoginActivity::class.java)
-                startActivity(intent)
-            }
-
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
         }
+
+    }
+
+    companion object {
+        private const val TAG = "com.example.thegreensshop_app.LoginActivity"
     }
 }
--- app/src/main/java/com/example/thegreensshop_app/SignupActivity.kt
+++ app/src/main/java/com/example/thegreensshop_app/SignupActivity.kt
@@ -1,11 +1,17 @@
 package com.example.thegreensshop_app
 
-
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import java.util.*
+
 
 class SignupActivity : AppCompatActivity() {
 
@@ -18,39 +24,69 @@ class SignupActivity : AppCompatActivity() {
 
         auth = FirebaseAuth.getInstance()
 
-        val usernameEditText: EditText = findViewById(R.id.username_edittext)
+        val flatHouseNoEditText: EditText = findViewById(R.id.flat_houseNo_edittext)
         val firstNameEditText: EditText = findViewById(R.id.firstname_edittext)
         val lastNameEditText: EditText = findViewById(R.id.lastname_edittext)
         val emailEditText: EditText = findViewById(R.id.email_edittext)
         val passwordEditText: EditText = findViewById(R.id.password_edittext)
-        val addressEditText: EditText = findViewById(R.id.address_edittext)
         val streetEditText: EditText = findViewById(R.id.street_edittext)
         val cityEditText: EditText = findViewById(R.id.city_edittext)
         val zipcodeEditText: EditText = findViewById(R.id.zipcode_edittext)
         val phoneEditText: EditText = findViewById(R.id.phone_edittext)
 
-
-
         val signupButton: Button = findViewById(R.id.signup_button)
-
 
         signupButton.setOnClickListener {
             val firstName = firstNameEditText.text.toString()
             val lastName = lastNameEditText.text.toString()
-            val user = "$firstName $lastName"
-            val address = addressEditText.text.toString()
-            val street = streetEditText.text.toString()
-            val city = cityEditText.text.toString()
-            val zipcode = zipcodeEditText.text.toString()
-           // val lat = latEditText.text.toString()
-            //val long = longEditText.text.toString()
-          //  val geolocation = "$lat,$long"
+            val name = Name(firstName, lastName)
+            val address = Address(flatHouseNoEditText.text.toString(), streetEditText.text.toString(), cityEditText.text.toString(), zipcodeEditText.text.toString(), "Ireland")
             val phone = phoneEditText.text.toString()
             val email = emailEditText.text.toString()
             val password = passwordEditText.text.toString()
 
+            // Generate random geolocation
+            val random = Random()
+            val minLat = -90.0
+            val maxLat = 90.0
+            val minLong = -180.0
+            val maxLong = 180.0
+            val lat = minLat + (maxLat - minLat) * random.nextDouble()
+            val long = minLong + (maxLong - minLong) * random.nextDouble()
+            val geolocation = "$lat,$long"
 
-            // TODO: enviar objeto User para o servidor para registro
+            val newUser = User(name, email, phone, address, geolocation)
+
+            // Save user object to Firestore
+            val db = FirebaseFirestore.getInstance()
+            db.collection("users").document(email).set(newUser)
+                .addOnSuccessListener {
+                    Log.d(TAG, "User created successfully!")
+                    Toast.makeText(this, "User created successfully!", Toast.LENGTH_SHORT).show()
+
+                    // Sign up user with Firebase Authentication
+                    auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this) { task ->
+                            if (task.isSuccessful) {
+                                Log.d(TAG, "User signed up with Firebase Authentication successfully!")
+                                Toast.makeText(this, "User signed up with Firebase Authentication successfully!", Toast.LENGTH_SHORT).show()
+                                val intent = Intent(this, MainActivity::class.java)
+                                startActivity(intent)
+                            } else {
+                                Log.w(TAG, "Error signing up user with Firebase Authentication", task.exception)
+                                Toast.makeText(this, "Error signing up user with Firebase Authentication", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                }
+                .addOnFailureListener { e ->
+                    Log.w(TAG, "Error creating user", e)
+                    Toast.makeText(this, "Error creating user", Toast.LENGTH_SHORT).show()
+                }
         }
     }
+
+    data class Name(val firstName: String, val lastName: String)
+    data class Address(val flatHouseNo: String, val street: String, val city: String, val zipcode: String, val country: String)
+    data class User(val name: Name, val email: String, val phone: String, val address: Address, val geolocation: String)
 }
+
